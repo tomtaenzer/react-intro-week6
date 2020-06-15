592d030b0fabd7b2eaf54f495f999192da22fb16
Tom Tänzer <tomtaenzer@gmx.net>
2020-06-15 11:35:48 +0200

    Add List-Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Header from "./components/Header";
+import List from "./components/List";
+
+
 
 
 function App() {
   return (
-    <div className="App"><Header></Header></div>
+    <div className="App">
+      <Header></Header>
+      <List></List>
+    </div>
   );
 }
 
---
+++ src/components/List.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function List(){
+    return(
+        <ul></ul>
+    )
+}
+
+export default List;
