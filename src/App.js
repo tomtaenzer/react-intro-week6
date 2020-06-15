7cf199357ac751968c94aab598536b301d710443
Tom Tänzer <tomtaenzer@gmx.net>
2020-06-15 11:28:03 +0200

    Change Header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-
+import Header from "./components/Header";
 
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <div className="App"><Header></Header></div>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Header(){
+    return (
+        <header>
+            <h1>
+                Anybodys favorites Beers
+            </h1>
+        </header>
+    )
+}
