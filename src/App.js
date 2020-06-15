73c7d2fb6742c325c941ec505ba42fcb61dbac4d
Tom Tänzer <tomtaenzer@gmx.net>
2020-06-15 15:31:43 +0200

    Add Buttons

--- src/App.js
+++ src/App.js
@@ -3,17 +3,33 @@ import Header from "./components/Header";
 import List from "./components/List";
 import Footer from "./components/Footer";
 import Main from "./components/Main";
-const beers = ["lauwarmes Dortmunder Union", "Päffgen", "Andechser Hell"];
+
 
 const fastFood = ["Currywurst", "Pizza", "Döner"];
 
+const animals = ["Tiger", "Ape", "Dog", "Cat", "Penguin"];
+
 
 export default function App() {
+
+    const [itemsToDisplay, setItemsToDisplay] = React.useState(animals);
+
+    function toggleFoods() {
+        setItemsToDisplay(fastFood);
+    }
+
+    function toggleAnimals() {
+        setItemsToDisplay(animals);
+    }
+
   return (
     <div className="App">
       <Header>Toms possible Lunch</Header>
         <Main>
-            <List items={fastFood} />
+            <button onClick={toggleAnimals}>show Animals!</button>
+            <button onClick={toggleFoods}>show Foods!</button>
+            <List items={itemsToDisplay} />
+
         </Main>
       <Footer>Made with appetite</Footer>
     </div>
